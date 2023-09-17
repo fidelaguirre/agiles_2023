1252df2314d5ddec7e308bd9e0624b71d55663fa
fidelaguirre <fidelaguirre1998@gmail.com>
2023-09-17 20:25:52 -0300

    Se instaló Amplify

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ed280ff9c495c1fa"; }
+/******/ 		__webpack_require__.h = function() { return "cb86300d7ef013ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
