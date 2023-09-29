fa20e80a304c6b5c5acfa27bdf34a6d4fb9dd6dc
Torres Martín <martin.torres98@hotmail.com>
2023-09-28 21:02:54 -0300

    Amplify backend update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4689aa87123359ef"; }
+/******/ 		__webpack_require__.h = function() { return "75ab7a401565e7f5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
