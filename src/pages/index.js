193c2616e6fcf20298cbf2b4bf673978a6ca22f5
Juan Cruz Moyano <jcrzmoyano@gmail.com>
2023-10-19 21:18:24 -0300

    se agrega Navbar al Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,17 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import { Auth } from 'aws-amplify';
-import { Menu, MenuItem } from "@aws-amplify/ui-react";
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import Layout from "./components/Layout";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
-  async function signOut() {
-    try {
-      await Auth.signOut({ global: true });
-    } catch (error) {
-      console.log('error signing out: ', error);
-    }
-  }
-
   return (
     <>
-      <Head>
-        <title>SoundCollab</title>
-        <meta name="description" content="Conéctate con músicos, productores y más." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* <a type='button' id="MutationEventButton" onClick={createNewBlog}>Testeando ando</a>
+      <Layout>
+        {/* <a type='button' id="MutationEventButton" onClick={createNewBlog}>Testeando ando</a>
       <p id="MutationResult">{resp ? resp.data.createBlog.name : ""}</p> */}
-      <h2>Header</h2>
-          <Menu menuAlign="end">
-            <MenuItem isDisabled onClick={() => alert("Perfil")}>
-              Perfil
-            </MenuItem>
-            <MenuItem onClick={signOut}>Cerrar Sesión</MenuItem>
-          </Menu>
+        <body>body</body>
+      </Layout>
     </>
-  )
+  );
 }
